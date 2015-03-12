6f485e131d62e9815a60e1cd8b2f573cb207c50c
William Jagels <will.jagels@gmail.com>
2015-03-12 19:10:18 -0400

    Just about done

--- assignment07/Driver.java
+++ assignment07/Driver.java
@@ -6,11 +6,22 @@ public class Driver {
 	public static int[] getArray(int max) {
 		int[] arr = new int[97];
 		for(int i=0;i<max;i++) {
+			String is = Integer.toString(i);//wow, much optimize
 			arr[0] += i%2;
 			arr[1] += i%2 == 0 ? 1 : 0;
 			arr[2] += isPrime(i) ? 1 : 0;
-			for(int k=1;k<=20)
-				arr[k+2] += divisibility(i, k);
+			for(int k=1;k<=20;k++)
+				arr[k+2] += div(i, k);
+			for(int p=1;p<=10;p++) {
+				arr[22 + (p*(p-1)/2 +1) + i%p]++;
+			}
+			if(i != 0)
+				arr[77 + Character.getNumericValue(is.charAt(0))]++;
+			for(int q=0;q<10;q++){
+				if(is.contains(Integer.toString(q))){
+					arr[87 + q] += 1;
+				}
+			}
 		}
 		System.out.println(Arrays.toString(arr));
 		return arr;
@@ -27,8 +38,10 @@ public class Driver {
 		return true;
 	}
 
-	private static int divisibility(int num, int mod) {
-		if()
+	private static int div(int num, int mod) {
+		if(num % mod == 0)
+			return 1;
+		return 0;
 	}
 
 }
